067d2db63da671e9c8bb327c7f63ec469978bb62
Jas <jaswithareddy@uchicago.edu>
2023-02-19 00:42:56 -0600

    DataCache-1

--- Project6/Model/MovieClient.swift
+++ Project6/Model/MovieClient.swift
@@ -16,11 +16,24 @@ class MovieClient {
     ///     - completion: A tuple with an `Array` of the movies and an error code
     /// - Throws:
     /// - Returns:
+    
+    static var cachedMovies: movielist?
+    
     static func fetchMovies(url: String, completion: @escaping (movielist?, Error?) -> Void) {
         print("DEBUG ---> Attempting to fetch movies")
+        
+        // checking if cached results already exist
+        if let cachedMovies = cachedMovies {
+            print("DEBUG ---> Returning cached movies")
+            DispatchQueue.main.async {
+                completion(cachedMovies, nil)
+                return
+            }
+        }
+        
         let defaulturl = URL(string: "https://itunes.apple.com/search?country=US&media=movie&limit=200&term=love")!
         let url = URL(string: url) ?? defaulturl
-        print("Used",url)
+
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             
             guard let data = data, error == nil else {
@@ -33,6 +46,9 @@ class MovieClient {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let issues = try decoder.decode(movielist.self, from: data)
                 print("DEBUG ------> fetched new movies:", issues)
+                
+                // updating cached results
+                cachedMovies = issues
                 DispatchQueue.main.async { completion(issues, nil) }
             } catch(let parsingError) {
                 DispatchQueue.main.async { completion(nil, parsingError) }
